d3a9f7a782fe6a1ebc03f94427ee4522f97d2ebf
Humberto Anjos <h.anjos@gmail.com>
2011-12-20 15:32:28 -0200

    First attempt at testing with NUnit.

---
+++ GameOfLife/Tests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Tests
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //string[] my_args = { Assembly.GetExecutingAssembly().Location };
+
+            //int returnCode = NUnit.ConsoleRunner.Runner.Main(my_args);
+
+            //if (returnCode != 0)
+            //    Console.Beep();
+
+            AppDomain.CurrentDomain.ExecuteAssembly(
+                @"C:\Arquivos de programas\NUnit 2.5.10\bin\net-2.0\NUnit-console.exe",
+                null,
+                new string[] { Assembly.GetExecutingAssembly().Location });
+        }
+    }
+}
---
+++ GameOfLife/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyInfo")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9f7363c-b169-4413-b521-33e7357502bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
