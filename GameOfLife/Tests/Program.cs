b868292b3fc9476343316452ebae479c5a8c8491
Humberto Anjos <h.anjos@gmail.com>
2011-12-22 08:22:47 -0200

    The libraries were missing...

---
+++ GameOfLife/Tests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace Tests
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            NUnit.Gui.AppEntry.Main(new string[] 
+            {
+              Assembly.GetExecutingAssembly().Location, 
+              "/runselected"
+            });
+        }
+    }
+}
